70df9775125dd5f93760a6c67d848cd9a95e479b
caden <cpete.est2014@gmail.com>
2024-02-16 10:10:48 -0700

    calculator

---
+++ IntData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class IntData : ScriptableObject
+{
+    public int value;
+    
+    public void SetValue(int num)
+    {
+        value = num;
+    }
+  
+    public void UpdateValue(int num)
+    {
+        value += num;
+    }
+}
